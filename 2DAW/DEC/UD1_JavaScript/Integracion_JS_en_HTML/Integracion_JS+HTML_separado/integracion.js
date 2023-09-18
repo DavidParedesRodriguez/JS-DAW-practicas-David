0baea799466dce1da071074b9ef2d29fef3cbdad
DavidParedesRodriguez <deividregalon2002@gmail.com>
2023-09-18 23:36:45 +0200

    ejercicioSumarNumeroJSyHTMLseparadoHecho

---
+++ 2DAW/DEC/UD1_JavaScript/Integracion_JS_en_HTML/Integracion_JS+HTML_separado/integracion.js
@@ -0,0 +1,13 @@
+function suma(){
+
+    var numero1 = parseFloat(prompt("Introduce el primer número: "));
+    var numero2 = parseFloat(prompt("Introduce el segundo número: "));
+
+    var resultado = numero1 + numero2;
+
+    alert("El resultado de la suma es: "+resultado);
+}
+
+function mostrarResultado(){
+    suma();
+}
