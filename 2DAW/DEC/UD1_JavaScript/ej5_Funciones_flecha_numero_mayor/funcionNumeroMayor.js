5503768d99265a762e647881377be74303c0186f
DavidParedesRodriguez <deividregalon2002@gmail.com>
2023-09-19 09:02:41 +0200

    ej5_funcionNumeroMayorHecho

---
+++ 2DAW/DEC/UD1_JavaScript/ej5_Funciones_flecha_numero_mayor/funcionNumeroMayor.js
@@ -0,0 +1,9 @@
+const mayor = (numero) => {
+    if(numero > 20){
+        console.log(numero +" es myor que 20");
+    }else{
+        console.log(numero +" es menor que 20");
+    }
+}
+
+mayor(5);
