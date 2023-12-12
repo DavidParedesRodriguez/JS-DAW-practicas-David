f6ea51ea8de1ba66c7b466466df5ca890d448fa9
DavidParedesRodriguez <deividregalon2002@gmail.com>
2023-12-12 10:13:20 +0100

    ej2_Objetos_Hecho

---
+++ 2DAW/DEC/UD3_TYPESCRIPT/ej2_Objetos/Objetos.ts
@@ -0,0 +1,49 @@
+/*1. leer la teoría referente a los objetos.
+2. Crea un objeto del curso de DAW debidamente tipado donde obtengamos el nombre del profesor, nombre del alumno, la edad, pais, fecha de nacimiento, si tiene mascota o no y un objeto anidado llamado trabajo que contendrá la empresa donde se trabaja y el sector.
+3. Saca por pantalla los resultados mediante un console log*/
+
+// Definición de tipos
+type Trabajo = {
+    empresa: string;
+    sector: string;
+  };
+  
+  type CursoDAW = {
+    profesor: string;
+    alumno: string;
+    edad: number;
+    pais: string;
+    fechaNacimiento: string;
+    tieneMascota: boolean;
+    trabajo?: Trabajo; 
+  };
+  
+  // Crear un objeto del curso de DAW
+  const cursoDAW: CursoDAW = {
+    profesor: "Toni",
+    alumno: "David Paredes Rodríguez",
+    edad: 21,
+    pais: "España",
+    fechaNacimiento: "2002-05-28",
+    tieneMascota: true,
+    trabajo: {
+      empresa: "GFT",
+      sector: "Programador",
+    },
+  };
+  
+  // Uso del objeto
+console.log("Curso DAW:");
+console.log("Profesor:", cursoDAW.profesor);
+console.log("Alumno:", cursoDAW.alumno);
+console.log("Edad:", cursoDAW.edad);
+console.log("País:", cursoDAW.pais);
+console.log("Fecha de Nacimiento:", cursoDAW.fechaNacimiento);
+console.log("Tiene Mascota:", cursoDAW.tieneMascota ? "Sí" : "No");
+
+if (cursoDAW.trabajo) {
+  console.log("Trabajo:");
+  console.log("Empresa:", cursoDAW.trabajo.empresa);
+  console.log("Sector:", cursoDAW.trabajo.sector);
+}
+  
